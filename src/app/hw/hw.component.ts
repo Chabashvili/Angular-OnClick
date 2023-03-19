f87745bbf99e48f5d6d306d45338ec3b7044225f
Chabashvili <sandro.chabashvili.88@gmail.com>
2023-03-19 17:21:15 +0400

    Angular OnClick

---
+++ src/app/hw/hw.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hw',
+  templateUrl: './hw.component.html',
+  styleUrls: ['./hw.component.css']
+})
+export class HwComponent {
+
+}
